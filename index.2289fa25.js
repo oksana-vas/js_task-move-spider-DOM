af8f796b0bbbb21bdaa1df9652228cf362d397f3
Oksana Vasyliuk <oksana.vasyliuk13@gmail.com>
2024-04-15 16:40:46 +0200

    make build

---
+++ index.2289fa25.js
@@ -0,0 +1,3 @@
+const t=document.querySelector(".wall"),e=document.querySelector(".spider"),i=e.clientWidth,l=e.clientHeight;t.addEventListener("click",c=>{let n=t.clientWidth-i,a=t.clientHeight-l,h=c.offsetX-i/2,o=c.offsetY-l/2;e.style.left=`${Math.max(Math.min(h,n),0)}px`,e.style.top=`${Math.max(Math.min(o,a),0)}px`});//# sourceMappingURL=index.2289fa25.js.map
+
+//# sourceMappingURL=index.2289fa25.js.map
